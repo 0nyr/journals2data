daa6a61247da3c4983294d3ca01db9b47ceccdc6
0nyr* <rascoussier.florian@gmail.com>
2021-07-01 15:20:56 +0200

    BERT integration, path modifications and cleaning.

--- src/journals2data/scraper/url_predict/bertpredict.py
+++ src/journals2data/scraper/url_predict/bertpredict.py
@@ -4,23 +4,29 @@ from transformers import TFDistilBertForSequenceClassification
 import tensorflow as tf
 
 from journals2data import utils
+from journals2data import data
 
-def apply_BERT_prediction(dataframe: pd.DataFrame) -> pd.DataFrame:
+def apply_BERT_prediction(
+    dataframe: pd.DataFrame,
+    source: data.Source
+) -> pd.DataFrame:
     """
     Apply BERT prediction layer based on BERT classifier.
     """
+
+    # get model_path depending on the language
+    model_dirpath: str = utils.Global.BASE_BERT_MODEL_BASEPATH + \
+        utils.Global.BERT_LANGUAGE_DIRS[source.language]
+    model = TFDistilBertForSequenceClassification.from_pretrained(
+        model_dirpath
+    )
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 
     def predict(model, tokenizer, sentence):
         predict_input = tokenizer.encode(sentence, truncation=True, padding=True, return_tensors="tf")
         tf_output = model.predict(predict_input)[0]
         tf_prediction = tf.nn.softmax(tf_output, axis=1).numpy()[0]
         return tf_prediction[1]
-
-    # FIXME : Fix model path here,
-    # WARN: model path depends on the language!
-    model = TFDistilBertForSequenceClassification.from_pretrained(
-        '/home/cboscher/psat-elod_time_interval/psat-elod_time_interval/PSAT-master/PSAT-master/models/BERT_classifier_en')
-    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 
     dataframe['BERT'] = 0 # add column for results
     dataframe['BERT'] = dataframe.apply(
